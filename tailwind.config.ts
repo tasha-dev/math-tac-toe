e67e9dc1e97464483057c36a6888a6e51ba07303
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-03-05 16:25:54 +0330

    removed shadcn to start over with it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,17 @@
+// Codes by mahdi tasha
+// Importing part
 import type { Config } from "tailwindcss"
 
+// Defining configs of tailwindCSS
 const config = {
-  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './app/*.{tsx,ts,js,jsx}',
+    './components/ui/*.{tsx,ts,js,jsx}',
+    './components/*.{tsx,ts,js,jsx}',
   ],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  theme: {},
+  plugins: [],
+};
 
+// Exporting the configs
 export default config
